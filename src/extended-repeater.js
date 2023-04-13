dda8cd3d3ad4fd7a0d0cb2cd5cb0aaf1eb63185d
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-04-13 07:51:26 +0300

    feat: add solution Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,107 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let strUp = String(str);
+  let additionUp = String(options.addition);
+  let isAddition = Object.hasOwn(options, 'addition')
+  let resStr = [];
+
+  if ((options.repeatTimes === 1 || !options.repeatTimes) && !isAddition) {
+    return str;
+  }
+
+  if (
+    (options.repeatTimes === 1 || !options.repeatTimes) &&
+    isAddition &&
+    (!options.additionRepeatTimes || options.additionRepeatTimes < 2)
+  ) {
+    resStr.push(strUp);
+    resStr.push(additionUp);
+  }
+
+  if (
+    (options.repeatTimes === 1 || !options.repeatTimes) &&
+    isAddition &&
+    options.additionRepeatTimes >= 2
+  ) {
+    resStr.push(strUp);
+    for (let j = 0; j < options.additionRepeatTimes - 1; j++) {
+      resStr.push(additionUp);
+      resStr.push(options.additionSeparator ? options.additionSeparator : '|');
+    }
+    resStr.push(additionUp);
+  }
+
+  if (
+    options.repeatTimes &&
+    options.repeatTimes > 1 &&
+    !isAddition
+  ) {
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      resStr.push(strUp);
+      resStr.push(options.separator ? options.separator : '+');
+    }
+    resStr.push(strUp);
+  }
+
+  if (
+    (options.repeatTimes &&
+      options.repeatTimes > 1 &&
+      isAddition &&
+      !options.additionRepeatTimes) ||
+    options.additionRepeatTimes < 2
+  ) {
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      resStr.push(strUp);
+      resStr.push(additionUp);
+      resStr.push(options.separator ? options.separator : '+');
+    }
+    resStr.push(strUp);
+    resStr.push(additionUp);
+  }
+
+  if (
+    options.repeatTimes &&
+    options.repeatTimes > 1 &&
+    isAddition &&
+    options.additionRepeatTimes >= 2
+  ) {
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      resStr.push(strUp);
+      for (let j = 0; j < options.additionRepeatTimes - 1; j++) {
+        resStr.push(additionUp);
+        resStr.push(
+          options.additionSeparator ? options.additionSeparator : '|'
+        );
+      }
+      resStr.push(additionUp);
+      resStr.push(options.separator ? options.separator : '+');
+    }
+    resStr.push(strUp);
+    for (let j = 0; j < options.additionRepeatTimes - 1; j++) {
+      resStr.push(additionUp);
+      resStr.push(options.additionSeparator ? options.additionSeparator : '|');
+    }
+    resStr.push(additionUp);
+  }
+
+  return resStr.join('');
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
